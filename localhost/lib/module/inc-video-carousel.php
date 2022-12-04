298db3db328d56f36fad152569f4a73c3ba7b2aa
rainvell <119740320+rainvell@users.noreply.github.com>
2022-12-04 12:34:01 +0300

    test
    Test

---
+++ localhost/lib/module/inc-video-carousel.php
@@ -0,0 +1,56 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class="video-con-container">
+						
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Конкурс проводится  с ... по ...</h2>
+								<h3>Оцениваются:
+актуальность и значимость темы;
+цельность (нравственность) замысла, преследующего прогрессивный характер последствий публикации, передачи;
+аргументированность и глубина раскрытия темы;
+резонансность материала;
+точность и доходчивость языка и стиля изложения, своеобразие методов журналистского творчества;
+профессионально-этический подход;
+доступность подачи;
+цикл материалов в различных жанрах (если речь идет не о подборке материалов одного жанра).
+</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			/div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-3'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/3.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+						
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
